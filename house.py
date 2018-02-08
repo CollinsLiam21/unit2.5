a6b5475b2ec06e57156f3cd5d1bfbe82cb870144
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-02-08 12:10:18 -0500

    Updated from Brython Server: 2/8/2018 12:10:18 PM

--- house.py
+++ house.py
@@ -1,1 +1,28 @@
+#Liam Collins
+#2/7/18
+#graphicsDemo.py - using ggame
 
+from ggame import *
+
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+
+blackOutline = LineStyle(1,black)
+
+redRectangle = RectangleAsset(200,100,blackOutline,red) #width, height, outline, fill
+blueCircle = CircleAsset(50,blackOutline,blue) #radius, outline, fill
+greenEllipse = EllipseAsset(100,50,blackOutline,green) #width, height, outline, fill
+blackLine = LineAsset(50,160,blackOutline) #x_endpoint, y_endpoint, lineStyle
+redTriangle = PolygonAsset([(75,75), (100,100), (50,100)],blackOutline,red) #endpoint, outline, fill
+text = TextAsset('Literature',fill=green, style='bold 40pt Times') #text, other options
+
+Sprite(redRectangle, (100,300))
+Sprite(blueCircle,(50,50))
+Sprite(greenEllipse,(200,50))
+Sprite(blackLine)
+Sprite(redTriangle, (175,200))
+Sprite(text, (200,400))
+
+App().run()
