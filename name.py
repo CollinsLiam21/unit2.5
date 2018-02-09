0d5a69e7458adbc16ec68ec16c7516720b3e5eca
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-02-09 08:45:51 -0500

    Updated from Brython Server: 2/9/2018 8:45:50 AM

--- name.py
+++ name.py
@@ -4,25 +4,17 @@
 
 from ggame import *
 
-red = Color(0xFF0000,1)
-green = Color(0x00FF00,1)
-blue = Color(0x0000FF,1)
-black = Color(0x000000,1)
+name = input('Enter your name: ')
+colorCode = input('Enter color code: ')
 
-blackOutline = LineStyle(1,black)
+color = Color(colorCode,1)
 
-redRectangle = RectangleAsset(200,100,blackOutline,red) #width, height, outline, fill
-blueCircle = CircleAsset(50,blackOutline,blue) #radius, outline, fill
-greenEllipse = EllipseAsset(100,50,blackOutline,green) #width, height, outline, fill
-blackLine = LineAsset(50,160,blackOutline) #x_endpoint, y_endpoint, lineStyle
-redTriangle = PolygonAsset([(75,75), (100,100), (50,100)],blackOutline,red) #endpoint, outline, fill
-text = TextAsset('Literature',fill=green, style='bold 40pt Times') #text, other options
+blackOutline = LineStyle(0,black)
 
-Sprite(redRectangle, (100,300))
-Sprite(blueCircle,(50,50))
-Sprite(greenEllipse,(200,50))
-Sprite(blackLine)
-Sprite(redTriangle, (175,200))
-Sprite(text, (200,400))
+Rectangle = RectangleAsset(1100,600,blackOutline,color) #width, height, outline, fill
+text = TextAsset(name,fill=black, style='bold 25pt Times') #text, other options
+
+Sprite(Rectangle)
+Sprite(text,(400,225))
 
 App().run()
