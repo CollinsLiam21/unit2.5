b044273af99321e06e9437176b0a4f9f34320d72
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-02-09 08:14:54 -0500

    Updated from Brython Server: 2/9/2018 8:14:53 AM

--- house.py
+++ house.py
@@ -11,18 +11,20 @@ black = Color(0x000000,1)
 
 blackOutline = LineStyle(1,black)
 
-redRectangle = RectangleAsset(200,100,blackOutline,red) #width, height, outline, fill
-blueCircle = CircleAsset(50,blackOutline,blue) #radius, outline, fill
-greenEllipse = EllipseAsset(100,50,blackOutline,green) #width, height, outline, fill
-blackLine = LineAsset(50,160,blackOutline) #x_endpoint, y_endpoint, lineStyle
-redTriangle = PolygonAsset([(75,75), (100,100), (50,100)],blackOutline,red) #endpoint, outline, fill
-text = TextAsset('Literature',fill=green, style='bold 40pt Times') #text, other options
+redRectangle = RectangleAsset(100,100,blackOutline,red) #width, height, outline, fill
+redRectangle2 = RectangleAsset(25,50,blackOutline,red)
+#blueCircle = CircleAsset(50,blackOutline,blue) #radius, outline, fill
+#greenEllipse = EllipseAsset(100,50,blackOutline,green) #width, height, outline, fill
+#blackLine = LineAsset(0,60,blackOutline) #x_endpoint, y_endpoint, lineStyle
+redTriangle = PolygonAsset([(100,100), (150,150), (50,150)],blackOutline,green) #endpoint, outline, fill
+#text = TextAsset('Literature',fill=green, style='bold 40pt Times') #text, other options
 
-Sprite(redRectangle, (100,300))
-Sprite(blueCircle,(50,50))
-Sprite(greenEllipse,(200,50))
-Sprite(blackLine)
-Sprite(redTriangle, (175,200))
-Sprite(text, (200,400))
+Sprite(redRectangle, (100,150))
+Sprite(redRectangle2, (135,200))
+#Sprite(blueCircle,(50,50))
+#Sprite(greenEllipse,(200,50))
+#Sprite(blackLine, (135,190))
+Sprite(redTriangle, (100,100))
+#Sprite(text, (200,400))
 
 App().run()
