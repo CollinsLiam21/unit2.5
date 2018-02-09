95f6dd9821dc1a1526db0d80f4ccb339aef36135
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-02-09 08:30:43 -0500

    Updated from Brython Server: 2/9/2018 8:30:42 AM

--- germany.py
+++ germany.py
@@ -1,1 +1,29 @@
+#Liam Collins
+#2/9/18
+#germany.py
 
+from ggame import *
+
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+yellow = Color(0xFFFF00,1)
+
+blackOutline = LineStyle(1,black)
+redOutline = LineStyle(1,red)
+yellowOutline = LineStyle(1,yellow)
+
+blackRectangle = RectangleAsset(300,50,blackOutline,black)
+redRectangle = RectangleAsset(300,50,redOutline,red) #width, height, outline, fill
+yellowRectangle = RectangleAsset(300,50,yellowOutline,yellow)
+
+
+text = TextAsset('Literature',fill=green, style='bold 40pt Times') #text, other options
+
+Sprite(blackRectangle, (100,100))
+Sprite(redRectangle, (100,150))
+Sprite(yellowRectangle, (100,200))
+
+
+App().run()
