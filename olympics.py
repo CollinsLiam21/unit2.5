6deba8d0da992918c55ca638e5f95574506fb8ba
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-03-07 10:04:24 -0500

    Updated from Brython Server: 3/7/2018 10:04:23 AM

--- olympics.py
+++ olympics.py
@@ -13,8 +13,17 @@ white = Color(0xFFFFFF,1)
 
 blueCircle = CircleAsset(100,LineStyle(1,blue),blue)
 whiteCircle = CircleAsset(80,LineStyle(1,white),white)
+blackCircle = CircleAsset(100,LineStyle(1,black),black)
+redCircle = CircleAsset(100,LineStyle(1,red),red)
+yellowCircle = CircleAsset(100,LineStyle(1,yellow),yellow)
 
 Sprite(blueCircle)
 Sprite(whiteCircle, (20,20))
+Sprite(blackCircle, (225,0))
+Sprite(whiteCircle, (245,20))
+Sprite(redCircle, (450,0))
+Sprite(whiteCircle, (470,20))
+Sprite(yellowCircle, (113,113))
+Sprite(whiteCircle, (133,133))
 
 App().run()
