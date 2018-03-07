8006f5c239153034320e3d9b41ffd0dac121b26d
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-03-07 08:40:14 -0500

    Updated from Brython Server: 3/7/2018 8:40:14 AM

--- olympics.py
+++ olympics.py
@@ -1,1 +1,20 @@
+#Liam Collins
+#3/7/18
+#olympics.py
 
+from ggame import *
+
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+red = Color(0xFF0000,1)
+yellow = Color(0xFFFF00,1)
+green = Color(0x00FF00,1)
+white = Color(0xFFFFFF,1)
+
+blueCircle = CircleAsset(100,LineStyle(1,blue),blue)
+whiteCircle = CircleAsset(80,LineStyle(1,white),white)
+
+Sprite(blueCircle)
+Sprite(whiteCircle, (20,20))
+
+App().run()
